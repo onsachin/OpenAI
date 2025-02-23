1f66bc930da7a27120273de956e2d6c8f7fe34db
Sachin <onsachin@icloud.com>
2025-02-23 23:13:02 +0530

    Sample chat app code added using GPM-4 model

---
+++ OpenAISamples/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+using Azure;  
+using Azure.AI.OpenAI;
+using OpenAI.Chat;
+
+public class AzureOpenAISample
+{
+    private string endpoint = "https://openai-dem.openai.azure.com/";
+    private string apiKey = "7PY6GYLBpKYd022bPk3BE1Dahe29SqBXsUAYGlSFnWFXFUvzNGmcJQQJ99BBACYeBjFXJ3w3AAABACOGbS5c";
+
+    private static void Main()
+    {
+        AzureOpenAISample client = new AzureOpenAISample();
+        client.CreateChatCompletion();
+    }
+
+    private void CreateChatCompletion()
+    {
+        var client = new AzureOpenAIClient(new Uri(endpoint), new AzureKeyCredential(apiKey));
+
+        ChatClient chatClient = client.GetChatClient("gpt-4");
+
+        var message = new List<ChatMessage>()
+        {
+            new SystemChatMessage(Console.ReadLine())
+        };
+
+        ChatCompletion completion = chatClient.CompleteChat(message);
+        Console.WriteLine(completion.Content.FirstOrDefault()?.Text);
+        return;
+    }
+
+}

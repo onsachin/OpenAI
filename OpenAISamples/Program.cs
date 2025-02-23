66faac2b19a3447df7547ac813b4226f89fd16e6
Sachin <onsachin@icloud.com>
2025-02-23 23:23:15 +0530

    code fixed

--- OpenAISamples/Program.cs
+++ OpenAISamples/Program.cs
@@ -5,29 +5,6 @@ using OpenAI.Chat;
 
 public class AzureOpenAISample
 {
-    private string endpoint ="";// type endpoint url 
-    private string apiKey = ""; //Type  azure api key";
-
-    private static void Main()
-    {
-        AzureOpenAISample client = new AzureOpenAISample();
-        client.CreateChatCompletion();
-    }
-
-    private void CreateChatCompletion()
-    {
-        var client = new AzureOpenAIClient(new Uri(endpoint), new AzureKeyCredential(apiKey));
-
-        ChatClient chatClient = client.GetChatClient("gpt-4");
-
-        var message = new List<ChatMessage>()
-        {
-            new SystemChatMessage(Console.ReadLine())
-        };
-
-        ChatCompletion completion = chatClient.CompleteChat(message);
-        Console.WriteLine(completion.Content.FirstOrDefault()?.Text);
-        return;
-    }
+    
 
 }
